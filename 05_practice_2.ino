076616653c170e6670ac2bbb05d43dd661a8ede2
luckyisjelly <133952441+luckyisjelly@users.noreply.github.com>
2023-09-24 23:03:11 +0900

    Update and rename .gitattributes to 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,33 @@
+#define PIN_7 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_7, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  count = toggle = 0;
+  digitalWrite(PIN_7, toggle); // turn off LED.
+  delay(1000);
+}
+
+void loop() {
+  Serial.println(++count);
+  
+  digitalWrite(PIN_7, HIGH);
+  delay(1000);
+  
+  for (int i = 0; i < 10; i++) {
+    toggle = toggle_state(toggle); // toggle LED value.
+    digitalWrite(PIN_7, toggle);
+    delay(200);
+  }
+  
+  digitalWrite(PIN_7, LOW); // turn off LED
+  
+  while(1) {}} // infinite loop
+
+int toggle_state(int toggle) {
+  return (toggle+1)%2;
+}
